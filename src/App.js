90c9bdf4bd593952c0fecd2aa2f88ad1bedd2885
Austinove <bryanovicaustenove@gmail.com>
2020-08-21 12:28:11 +0300

    initial react-app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          #Team-225-Group-a initial front-end
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/BuildForSDGCohort2/Team-225-Frontend"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Our repository
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
