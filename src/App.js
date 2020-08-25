9d1271928450434b568633dea8e4960de692752b
okorie2 <44427128+okorie2@users.noreply.github.com>
2020-08-25 16:36:28 +0100

    footer completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import Footer from './components/footerComponent/footer'
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <Footer/>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          #Team-225-Group-a initial front-end
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/BuildForSDGCohort2/Team-225-Frontend"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Our repository
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
