829abca6ac2b26fd265d9e31171468c7bcf732a2
Austinove <bryanovicaustenove@gmail.com>
2020-09-07 08:03:09 +0300

    created private routes function

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import React, { Component, Suspense, lazy } from "react";
-import { Router, Route, Switch } from "react-router-dom";
-import { history } from "../store";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { Jumbotron, Container } from "react-bootstrap";
+import { history } from "../store";
 import AppHeader from "./static/NavBar";
 import AppNavBar from "./static/NavBar/appNavBar";
 import Footer from "./static/Footer";
@@ -9,38 +9,51 @@ const HomePage = lazy(() => import("./views/homepage"));
 const Facilities = lazy(() => import("./views/facilities"));
 const Aboutpage = lazy(() => import("./views/aboutpage"));
 const ContactUs = lazy(() => import("./views/contactus"));
-const Rooms = lazy(() => import ("./views/rooms&rates"));
+const Rooms = lazy(() => import("./views/rooms&rates"));
 // const NotFound = lazy(() => import("./views/notFound"));
 
-class MainApp extends Component {
-    render() {
-        return (
-            <>
-                <AppHeader />
-                <AppNavBar />
-                <Suspense fallback={
-                    <Jumbotron fluid className="mt-4 mb-4 mr-auto ml-auto">
-                        <Container>
-                            <h1>Loading......</h1>
-                            <p>Please wait</p>
-                        </Container>
-                    </Jumbotron>
-                }>
-                    <Switch>
-                        <Router history={history}>
-                            <Route exact path="/" component={HomePage} />
-                            <Route exact path="/facilities" component={Facilities} />
-                            <Route exact path="/Rooms&Rates" component={Rooms} />
-                            <Route exact path="/contactUs" component={ContactUs} />
-                            <Route exact path="/aboutpage" component={Aboutpage} />
-                            {/* <Route exact path="*" component={NotFound} /> */}
-                            
-                        </Router>
-                    </Switch>
-                </Suspense>
-                <Footer />
-            </>
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      (JSON.parse(localStorage.getItem("user")) && JSON.parse(localStorage.getItem("user")).token) ? (
+        <Component {...props} />
+      ) : (
+          <Redirect to="/" />
         )
     }
+  />
+);
+
+class MainApp extends Component {
+  render() {
+    console.log(this.location);
+    return (
+      <>
+        <AppHeader />
+        <AppNavBar />
+        <Suspense fallback={
+          <Jumbotron fluid className="mt-4 mb-4 mr-auto ml-auto">
+            <Container>
+              <h1>Loading......</h1>
+              <p>Please wait</p>
+            </Container>
+          </Jumbotron>
+        }>
+          <Switch>
+            <Router history={history}>
+              <Route exact path="/" component={HomePage} />
+              <PrivateRoute exact path="/facilities" component={Facilities} />
+              <PrivateRoute exact path="/Rooms&Rates" component={Rooms} />
+              <PrivateRoute exact path="/contactUs" component={ContactUs} />
+              <PrivateRoute exact path="/aboutpage" component={Aboutpage} />
+              {/* <Route exact path="*" component={NotFound} /> */}
+            </Router>
+          </Switch>
+        </Suspense>
+        <Footer />
+      </>
+    )
+  }
 }
 export default MainApp;
--- src/components/static/NavBar/index.js
+++ src/components/static/NavBar/index.js
@@ -2,86 +2,87 @@ import React, { useState } from "react";
 import {
   Navbar,
   Nav,
-  InputGroup,
-  Form,
-  FormControl,
   Image,
-  Button
+  Button,
 } from "react-bootstrap";
 import imageLogo from "../../../assets/img/logo.JPG";
 import { useEffect } from "react";
 import Register from "../../views/auth/register";
 
 const AppHeader = () => {
-  const [scroll, setScroll] = useState(1)
+  const [scroll, setScroll] = useState(1);
   const [modalShow, setModalShow] = useState(false);
 
   useEffect(() => {
     document.addEventListener("scroll", () => {
-      const scrollCheck = window.scrollY < 100
+      const scrollCheck = window.scrollY < 100;
       if (scrollCheck !== scroll) {
-        setScroll(scrollCheck)
+        setScroll(scrollCheck);
       }
-    })
+    });
   });
-    return (
-      <>
-        <Navbar
-          collapseOnSelect
-          expand="lg"
-          variant="light"
-          className={scroll ? "navbar-custom fixed-top" : "navbar-custom fixed-top scrolled-nav"}
-        >
-          <Navbar.Brand href="/" className={scroll ? null : "scrolled-nav-items"}>
-            <Image src={imageLogo} height="50px" className="ml-4 mt-auto mb-auto" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" className={scroll ? null : "scrolled-nav-items"}/>
-          <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="ml-auto mr-4 nav-bar-custom font-14">
-                <Nav.Link href="/">HOME</Nav.Link>
-                <Nav.Link href="/rooms&rates">ROOMS&RATES</Nav.Link>
-                <Nav.Link href="/facilities">FACILITIES</Nav.Link>
-                <Nav.Link href="/contactUs">CONTACT US</Nav.Link>
-                <Nav.Link href="/aboutpage">ABOUT US</Nav.Link>
-                <Form inline className="mr-3 ml-4">
-                  <InputGroup size="sm" className="input-group-custom">
-                    <FormControl
-                      className="search-input"
-                      type="text"
-                      placeholder="search"
-                      size="sm"
-                    />
-                    <InputGroup.Append size="sm">
-                      <InputGroup.Text size="sm" className="input-icon-custom">
-                        <i className="fa fa-search" aria-hidden="true"></i>
-                      </InputGroup.Text>
-                    </InputGroup.Append>
-                  </InputGroup>
-                </Form>
-                <div className="btn-holder">
-                  <Button
-                    size="sm"
-                    className="mr-4 btn-custom font-12 custom-purple"
-                  >
-                    SIGN IN
-                  </Button>
-                  <Button
-                    variant="outline-info"
-                    size="sm"
-                    className="mr-4  btn-custom font-12"
-                    onClick={() => setModalShow(true)}
-                  >
-                    JOIN
-                  </Button>
-                </div>
-              </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        variant="light"
+        className={
+          scroll
+            ? "navbar-custom fixed-top"
+            : "navbar-custom fixed-top scrolled-nav"
+        }
+      >
+        
+        <Navbar.Brand href="/" className={scroll ? null : "scrolled-nav-items"}>
+          <Image
+            src={imageLogo}
+            height="50px"
+            className="ml-4 mt-auto mb-auto"
+          />
+        </Navbar.Brand>
+        <div className="custom-color d-lg-none d-xl-none">
+          Welcome: {" "} <strong className="custom-color">Bryan</strong>
+        </div>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          className={scroll ? null : "scrolled-nav-items"}
+        />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto mr-4 nav-bar-custom font-14">
+            <Nav.Link href="/">HOME</Nav.Link>
+            <Nav.Link href="/rooms&rates">ROOMS&RATES</Nav.Link>
+            <Nav.Link href="/facilities">FACILITIES</Nav.Link>
+            <Nav.Link href="/contactUs">CONTACT US</Nav.Link>
+            <Nav.Link href="/aboutpage">ABOUT US</Nav.Link>
+            <Nav.Link href="/" className="custom-color">
+              HELLO: <strong className="custom-color">Bryan</strong>
+            </Nav.Link>
+            <div className="btn-holder">
+              <Button
+                size="sm"
+                className="mr-4 btn-custom font-12 custom-purple"
+              >
+                SIGN IN
+              </Button>
+              <Button
+                variant="outline-info"
+                size="sm"
+                className="mr-4  btn-custom font-12"
+                onClick={() => setModalShow(true)}
+              >
+                JOIN
+              </Button>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+        
+      </Navbar>
 
-        {/* Registration Modal */}
-        <Register show={modalShow} onHide={() => setModalShow(false)} />
-      </>
-    );
-}
+      {/* Registration Modal */}
+      <Register show={modalShow} onHide={() => setModalShow(false)} />
+    </>
+  );
+};
 
 export default AppHeader;
