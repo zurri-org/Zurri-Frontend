caef9ce893abca45cfbe0f54837b275250173e06
Bryan <37239421+Austinove@users.noreply.github.com>
2020-09-25 16:30:03 +0300

    Homepage (#48)
    * installed helpful libralies
    
    * initial nav-header
    
    * structured project folders
    
    * created the app store
    
    * created the rootreducer
    
    * connected to store
    
    * connected the routes
    
    * connected all routes
    
    * routing functionality
    
    * installed react-bootstrap
    
    * Routes/redux (#11)
    
    * installed helpful libralies
    
    * initial nav-header
    
    * structured project folders
    
    * created the app store
    
    * created the rootreducer
    
    * connected to store
    
    * connected the routes
    
    * connected all routes
    
    * routing functionality
    
    * nav color
    
    * created App nav-bar
    
    * initial styling of the top app-header
    
    * created App nav-bar styling
    
    * restructured the footer
    
    * created facilities, Rooms, and contactsUs template screens
    
    * refactored the screens
    
    * fixed small screen nav-bars
    
    * fixed nav-bar and change on scroll
    
    * initializing nav-bar for scroll
    
    * added background image section
    
    * created aboutUs section
    
    * remaining landing page section
    
    * created registration modal
    
    * form validation with formik
    
    * created authentication actions
    
    * connected redux thunk
    
    * connected to API using axios/redux-thunk
    
    * resolved conflicts
    
    * fixed routes
    
    * fixed small screen nav-bar
    
    * fixed aboout-us page warnings
    
    * fixed the back nav-bar
    
    * app nav-bar styling
    
    * created private routes function
    
    * completed signup functionality
    
    * simple front-end route restriction
    
    * nav-bar access control
    
    * removed templary file
    
    * fixed sigin and register button actions
    
    * created login_request action and loginconstants
    
    * created login reducers
    
    * catch error from API
    
    * created loader for auth forms
    
    * made logout user functional
    
    * notifications for the user submitions
    
    * refactored code
    
    * created home page
    
    * refactored code for reusability
    
    * chnges links to more buttons
    
    * defined public route and fixing errors
    
    * setUp nav bar
    
    * Home page template and route links
    
    * templary linking hotem page
    
    * created flights template
    
    * created home page forms
    
    * home images links
    
    * created buttons
    
    * created the journy help section
    
    Co-authored-by: Isiko Saidiali <54436062+SaidiAli@users.noreply.github.com>

--- src/components/views/homepage/index.js
+++ src/components/views/homepage/index.js
@@ -1,25 +1,347 @@
 import React from "react";
-import { Jumbotron, Container } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Card,
+  Form,
+  InputGroup,
+} from "react-bootstrap";
+import mapImage from "../../../assets/img/mapsnip.JPG";
+import airImage from "../../../assets/img/airsnip.JPG";
+import panelImage from "../../../assets/img/panel.JPG";
 
 const HomePage = () => {
-    return (
-      <>
-        <Container className="top-section">
-          <Jumbotron>
-            <h1>Home Page!</h1>
-            <p>This is the space for Home page</p>
-            <p>Its under development please...</p>
-          </Jumbotron>
+  return (
+    <>
+      <section className="row home-section">
+        <Container className="mt-4 d-inline">
+          <Row>
+            <Col sm={5} xs={12} md={5} className="mx-auto">
+              <Card className="home-card p-4">
+                <Card.Body>
+                  <Card.Title className="text-center">
+                    Find Flight From Uganda
+                  </Card.Title>
+                  <Form>
+                    <Form.Row className="mt-2">
+                      <Col>
+                        <Form.Label className="font-13 text-mute">
+                          From:
+                        </Form.Label>
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text className="home-prepend font-12">
+                              <i
+                                className="fa fa-map-marker"
+                                aria-hidden="true"
+                              ></i>
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            size="md"
+                            className="home-input custom-input"
+                            placeholder="CITY OR AIRPORT"
+                          />
+                        </InputGroup>
+                      </Col>
+                      <Col>
+                        <Form.Label className="font-13 text-mute">
+                          To:
+                        </Form.Label>
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text className="home-prepend font-12">
+                              <i
+                                className="fa fa-map-marker"
+                                aria-hidden="true"
+                              ></i>
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            size="md"
+                            className="home-input custom-input"
+                            placeholder="CITY OR AIRPORT"
+                          />
+                        </InputGroup>
+                      </Col>
+                    </Form.Row>
+                    <Form.Row className="mt-2">
+                      <Col>
+                        <Form.Label className="font-13 text-mute">
+                          Depart:
+                        </Form.Label>
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text className="home-prepend font-12">
+                              <i
+                                className="fa fa-location-arrow"
+                                aria-hidden="true"
+                              ></i>
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            size="md"
+                            className="home-input custom-input"
+                            placeholder="CITY OR AIRPORT"
+                          />
+                        </InputGroup>
+                      </Col>
+                      <Col>
+                        <Form.Label className="font-13 text-mute">
+                          Arrival:
+                        </Form.Label>
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text className="home-prepend font-12">
+                              <i
+                                className="fa fa-arrow-down"
+                                aria-hidden="true"
+                              ></i>
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            size="md"
+                            className="home-input custom-input"
+                            placeholder="CITY OR AIRPORT"
+                          />
+                        </InputGroup>
+                      </Col>
+                    </Form.Row>
+                    <Form.Row className="mt-2">
+                      <Col>
+                        <Form.Label className="font-13 text-mute">
+                          Depart:
+                        </Form.Label>
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text className="home-prepend font-12">
+                              <i
+                                className="fa fa-location-arrow"
+                                aria-hidden="true"
+                              ></i>
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            size="md"
+                            className="home-input custom-input"
+                            placeholder="FIRST CLASS"
+                          />
+                        </InputGroup>
+                      </Col>
+                      <Col className="mt-2">
+                        <Button className="search-btn mt-4" size="md">
+                          {" "}
+                          Search
+                        </Button>
+                      </Col>
+                    </Form.Row>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col sm={5} xs={12} md={5} className="mx-auto">
+              <Card className="home-card p-4">
+                <Card.Body>
+                  <Card.Title className="text-center">
+                    Find Hotels In Uganda
+                  </Card.Title>
+                  <Form>
+                    <Form.Row className="mt-2">
+                      <Col>
+                        <Form.Label className="font-13 text-mute">
+                          Checkin:
+                        </Form.Label>
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text className="home-prepend font-12">
+                              <i
+                                className="fa fa-map-marker"
+                                aria-hidden="true"
+                              ></i>
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            size="md"
+                            className="home-input custom-input"
+                            placeholder="CITY OR AIRPORT"
+                          />
+                        </InputGroup>
+                      </Col>
+                      <Col>
+                        <Form.Label className="font-13 text-mute">
+                          Checkout:
+                        </Form.Label>
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text className="home-prepend font-12">
+                              <i
+                                className="fa fa-map-marker"
+                                aria-hidden="true"
+                              ></i>
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            size="md"
+                            className="home-input custom-input"
+                            placeholder="CITY OR AIRPORT"
+                          />
+                        </InputGroup>
+                      </Col>
+                    </Form.Row>
+                    <Form.Row className="mt-2">
+                      <Col>
+                        <Form.Label className="font-13 text-mute">
+                          Guests:
+                        </Form.Label>
+                        <InputGroup>
+                          <InputGroup.Prepend>
+                            <InputGroup.Text className="home-prepend font-12">
+                              <i
+                                className="fa fa-location-arrow"
+                                aria-hidden="true"
+                              ></i>
+                            </InputGroup.Text>
+                          </InputGroup.Prepend>
+                          <Form.Control
+                            size="md"
+                            className="home-input custom-input"
+                            placeholder="FIRST CLASS"
+                          />
+                        </InputGroup>
+                      </Col>
+                      <Col className="mt-2">
+                        <Button className="search-btn mt-4" size="md">
+                          {" "}
+                          Search
+                        </Button>
+                      </Col>
+                    </Form.Row>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </Container>
-        <Container className="top-section">
-          <Jumbotron>
-            <h1>Home Page!</h1>
-            <p>This is the space for Home page</p>
-            <p>Its under development please...</p>
-          </Jumbotron>
-        </Container>
-      </>
-    );
-}
+      </section>
+
+      <Row>
+        <Col md={12}>
+          <Row className="pr-3">
+            <Col md={5} sm={5} className="ml-auto d-flex">
+              <div className="text-right m-auto">
+                <h2 className="custom-color">Connecting All Ends Of </h2>
+                <h2 className="custom-color">
+                  The World To <span className="home-color">Uganda</span>
+                </h2>
+              </div>
+            </Col>
+            <Col md={5} sm={5} className="mr-auto">
+              <img
+                src={mapImage}
+                alt="Map Illustration"
+                className="mr-4 mt-4 img-fluid"
+              />
+            </Col>
+          </Row>
+        </Col>
+        <Col md={12}>
+          <Row>
+            <Col md={6} sm={6} className="ml-auto">
+              <img
+                src={airImage}
+                className="img-fluid"
+                alt="Air Illustration"
+              />
+            </Col>
+            <Col md={4} sm={4} className="mr-auto d-flex">
+              <div className="text-left m-auto">
+                <h2 className="custom-color">Making Your Travel</h2>
+                <h2 className="custom-color">Plans Easy And</h2>
+                <h2 className="home-color">Seamless</h2>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={12} className="text-center">
+          <h2>Most Visted Places</h2>
+        </Col>
+        <Col md={8} className="m-auto">
+          <Row className="mx-auto d-flex text-center">
+            <Col md={3} sm={3} className="mr-4 d-flex mt-1 mb-2 custom-div">
+              <div className="text-right m-auto">
+                <h3>Kampala</h3>
+              </div>
+            </Col>
+            <Col md={3} sm={3} className="mr-4 d-flex mt-1 mb-2 custom-div">
+              <div className="text-right m-auto">
+                <h3>Entebbe</h3>
+              </div>
+            </Col>
+            <Col md={3} sm={3} className="mr-4 d-flex mt-1 mb-2 custom-div">
+              <div className="text-right m-auto">
+                <h3>Jinja</h3>
+              </div>
+            </Col>
+            <Col md={3} sm={3} className="mr-4 d-flex mt-1 mb-2 custom-div">
+              <div className="text-right m-auto">
+                <h3>Mbale</h3>
+              </div>
+            </Col>
+            <Col md={3} sm={3} className="mr-4 d-flex mt-1 mb-2 custom-div">
+              <div className="text-right m-auto">
+                <h3>Fort Portal</h3>
+              </div>
+            </Col>
+            <Col md={3} sm={3} className="mr-4 d-flex mt-1 mb-2 custom-div">
+              <div className="text-right m-auto">
+                <h3>The Rwenzori</h3>
+              </div>
+            </Col>
+            <Col md={3} sm={3} className="mr-4 d-flex mt-1 mb-2 custom-div">
+              <div className="text-right m-auto">
+                <h3>Karamoja</h3>
+              </div>
+            </Col>
+            <Col md={3} sm={3} className="mr-4 d-flex mt-1 mb-2 custom-div">
+              <div className="text-right m-auto">
+                <h3>Gulu</h3>
+              </div>
+            </Col>
+            <Col md={3} sm={3} className="mr-4 d-flex mt-1 mb-2 custom-div">
+              <div className="text-right m-auto">
+                <h3>Arua</h3>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+
+      <Row className="mt-2 mb-2">
+        <Col md={12}>
+          <Row>
+            <Col md={6} sm={6} className="ml-auto">
+              <img
+                src={panelImage}
+                className="img-fluid"
+                alt="Air Illustration"
+              />
+            </Col>
+            <Col md={4} sm={4} className="mr-auto d-flex">
+              <div className="text-left m-auto">
+                <h2 className="custom-color">Are You Failing To Plan For</h2>
+                <h2 className="custom-color">Your Journey, Let Us <span className="home-color">Help</span></h2>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default HomePage;
