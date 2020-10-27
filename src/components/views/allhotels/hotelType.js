cdd58b2ec7ee33648a7fbee7a4a30ee8c793d43c
Austinove <bryanovicaustenove@gmail.com>
2020-10-27 12:44:40 +0300

    connected to the api

--- src/components/views/allhotels/hotelType.js
+++ src/components/views/allhotels/hotelType.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Row, Container, Card, Col } from "react-bootstrap";
 import hotelImage from "../../../assets/img/bg1.jpg";
 const HotelType = ({ heading, sectionColor, appartments }) => {
+  console.log("fetched data---",appartments.data.hotels);
   return (
     <section className={sectionColor ? "section rooms-color" : "section"}>
       <Row className="mt-4 mb-8">
@@ -9,24 +10,26 @@ const HotelType = ({ heading, sectionColor, appartments }) => {
       </Row>
       <Container className="mt-3 mb-4 text-center">
         <Row>
-          {appartments.map((appartment, index) => (
-            <Col xs={12} sm={6} md={4} className="mb-4" key={index}>
-              <Card className="rooms-card">
-                <a href="/hotel">
-                  <Card.Img variant="top" src={hotelImage} />
-                </a>
-                <Card.Body>
-                  <Card.Text>
-                    {appartment.name}
-                    <br />
-                    <i className="mr-1 fa fa-star-o" aria-hidden="true"></i>
-                    <i className="mr-1 fa fa-star-o" aria-hidden="true"></i>
-                    <i className="mr-1 fa fa-star-o" aria-hidden="true"></i>
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
+          {appartments.data.hotels &&
+            !!appartments.data.hotels.length &&
+            appartments.data.hotels.map((appartment, index) => (
+              <Col xs={12} sm={6} md={4} className="mb-4" key={index}>
+                <Card className="rooms-card">
+                  <a href="/hotel">
+                    <Card.Img variant="top" src={hotelImage} />
+                  </a>
+                  <Card.Body>
+                    <Card.Text>
+                      {appartment.hotel_name}
+                      <br />
+                      <i className="mr-1 fa fa-star-o" aria-hidden="true"></i>
+                      <i className="mr-1 fa fa-star-o" aria-hidden="true"></i>
+                      <i className="mr-1 fa fa-star-o" aria-hidden="true"></i>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
         </Row>
       </Container>
     </section>
--- src/components/views/allhotels/index.js
+++ src/components/views/allhotels/index.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { Row, Form, Button, Col } from "react-bootstrap";
+import { Row, Form, Button, Col, Container, Jumbotron, Card } from "react-bootstrap";
 import { fetchHotels } from "../../../actions/hotelsActions";
-import HotelType from "./hotelType";
+import hotelImage from "../../../assets/img/bg1.jpg";
+// import HotelType from "./hotelType";
 const AllHotels = ({
   hotel_data,
   loading,
@@ -13,11 +14,18 @@ const AllHotels = ({
   useEffect(() => {
     fetchHotels();
   }, [fetchHotels]);
-  const appartments = [
-    { name: "Arsenal Suits" },
-    { name: "Chelsea Suits" },
-    { name: "Manchester United Suits" },
-  ];
+  // const appartments = [
+  //   { name: "Arsenal Suits" },
+  //   { name: "Chelsea Suits" },
+  //   { name: "Manchester United Suits" },
+  // ];
+  console.log(hotel_data)
+  console.log(hotel_data.length);
+  if(hotel_data.data !== undefined){
+  console.log('contnentsssss',hotel_data.data.hotels);
+  }else{
+    console.log("nothing");
+  }
   return (
     <>
       <Row className="text-center d-block home-search mb-4">
@@ -43,19 +51,67 @@ const AllHotels = ({
           </Form>
         </Col>
       </Row>
-
-      {/* Reusable component sections */}
-      <HotelType
-        heading="HOTELS"
-        sectionColor="rooms-color"
-        appartments={appartments}
-      />
-      <HotelType heading="RESORTS" sectionColor="" appartments={appartments} />
-      <HotelType
-        heading="MOTELS"
-        sectionColor="rooms-color"
-        appartments={appartments}
-      />
+      {loading === "Loading" ? (
+        <Jumbotron fluid className="top-section mb-4 mr-auto ml-auto">
+          <Container>
+            <h1>Loading......</h1>
+            <p>Please wait</p>
+          </Container>
+        </Jumbotron>
+      ) : (
+        <>
+          <section className="section rooms-color">
+            <Row className="mt-4 mb-8">
+              <h1 className="mt-4 section-header">HOTELS</h1>
+            </Row>
+            <Container className="mt-3 mb-4 text-center">
+              <Row>
+                {hotel_data.data !== undefined &&
+                  hotel_data.data.hotels.map((appartment, index) => (
+                    <Col xs={12} sm={6} md={4} className="mb-4" key={index}>
+                      <Card className="rooms-card">
+                        <a href="/hotel">
+                          <Card.Img
+                            variant="top"
+                            src={appartment.images[1].url}
+                          />
+                        </a>
+                        <Card.Body>
+                          <Card.Text>
+                            {appartment.hotel_name}
+                            <br />
+                            <i
+                              className="mr-1 fa fa-star-o"
+                              aria-hidden="true"
+                            ></i>
+                            <i
+                              className="mr-1 fa fa-star-o"
+                              aria-hidden="true"
+                            ></i>
+                            <i
+                              className="mr-1 fa fa-star-o"
+                              aria-hidden="true"
+                            ></i>
+                          </Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  ))}
+              </Row>
+            </Container>
+          </section>
+          {/* <HotelType
+            heading="RESORTS"
+            sectionColor=""
+            appartments={appartments}
+          />
+          <HotelType
+            heading="MOTELS"
+            sectionColor="rooms-color"
+            appartments={appartments}
+          /> */}
+        </>
+      )}
     </>
   );
 };
